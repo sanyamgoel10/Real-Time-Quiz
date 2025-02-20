b564841878313a90130998824f7507bc9513e026
Sanyam Goel <sanyamgoel10@gmail.com>
2025-02-20 08:53:19 +0530

    added search game

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,9 +1,12 @@
 class GameController{
     async landingpage(req, res){
+        return res.render('landingpage');
+    }
+
+    async searchGame(req, res){
         let userName = req.UserNameFromToken;
         console.log("userName: ", userName);
-        
-        return res.render('landingpage');
+        return res.render('searchgame');
     }
 }
 module.exports = new GameController();
--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
-const { landingpage } = require('../controllers/gameController.js');
+const { landingpage, searchGame } = require('../controllers/gameController.js');
 
 // Middleware
 const { verifyJwt } = require('../middleware/validateUserReq.js');
 
 // Routes
 router.get('/', verifyJwt, landingpage);
+router.get('/start', verifyJwt, searchGame)
 
 module.exports = router;
