4a26a6083008377033d0ace1c7d03d3be1d65e19
Sanyam Goel <sanyamgoel10@gmail.com>
2025-02-22 12:29:55 +0530

    removed server side code

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const config = require("./config/config.js");
 const PORT = config.port || 3000;
 const app = require("./app.js");
 
-// Websocket App
-const WebSocket = require('ws');
+// Initiate a http server
 const http = require('http');
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+
+// Import Websocket server
+const { initializeWebSocketServer } = require('./wsServer.js');
 
 const startServer = async () => {
     try {
@@ -20,33 +21,8 @@ const startServer = async () => {
         // Initialize questions in database
         await initializeQuestions();
         
-        // Websocket connection start
-        wss.on('connection', async (ws) => {
-            console.log('A new client has connected to websocket.');
-            ws.on('message', async (message) => {
-                console.log(`Received message from client`);
-
-                if('undefined' != typeof message){
-                    let reqMsg = JSON.parse(message);
-                    console.log("Message from client: ", reqMsg);
-                    if('undefined' != typeof reqMsg.action && 'undefined' != typeof reqMsg.username){
-                        let currAction = reqMsg.action;
-                        let userName = reqMsg.username;
-
-                        if(currAction == 'initialize'){
-
-                        }
-
-                        // Broadcast message
-                        // await broadcastMessageOnWs(WebSocket, wss, ws, message);
-                    }
-                }
-            });
-            // Websocket Connection Closing
-            ws.on('close', () => {
-                console.log('A client has disconnected.');
-            });
-        });
+        // Initialize a websocket server
+        // await initializeWebSocketServer(server);
 
         // Start Server
         server.listen(PORT, () => {
@@ -57,14 +33,5 @@ const startServer = async () => {
         process.exit(1);
     }
 };
-
-async function broadcastMessageOnWs(WebSocket, wss, ws, message){
-    let allClients = wss.clients;
-    allClients.forEach((client) => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(message);
-        }
-    });
-}
 
 startServer();
