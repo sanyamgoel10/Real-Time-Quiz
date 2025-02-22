18517ff5623bb1be506dd3fabd1bdf787e9d70e3
Sanyam Goel <sanyamgoel10@gmail.com>
2025-02-22 13:21:35 +0530

    check

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -8,20 +8,20 @@ class AuthMiddleware{
             if('undefined' == typeof pageCookies.userToken || pageCookies.userToken == null || 'string' != typeof pageCookies.userToken || (pageCookies.userToken).trim() == ''){
                 let errResp = `token not found in req`;
                 console.log(`Error: ${errResp}`);
-                return res.render('error');
+                return res.render('requiredlogin');
             }
 
             let verifiedToken = await verifyJwtToken(pageCookies.userToken);
             if(!verifiedToken || 'undefined' == typeof verifiedToken.username){
                 let errResp = `Invalid token in req`;
                 console.log(`Error: ${errResp}`);
-                return res.render('error');
+                return res.render('requiredlogin');
             }
             req.UserNameFromToken = verifiedToken.username;
             next();
         } catch (error) {
             console.log("Error: ", error);
-            return res.render('error');
+            return res.render('requiredlogin');
         }
     }
 }
