45659600c4fe27c35c09a599bf043a32aab4dc07
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2025-03-02 12:07:12 +0530

    Create config.js for deployment

---
+++ config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    port: 3000,
+    
+    dbHost: "localhost",
+    dbPort: 27017,
+    dbName: "quiz_db",
+
+    EncryptSaltRounds: 10,
+
+    JWT_SECRET_KEY: "jwt_secret_key_sg",
+    TOKEN_HEADER_KEY: "jwt_header_key_sg",
+
+    MaxQuizQuestions: 4,
+}
