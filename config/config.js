b4f66b5e86f9796dbb5d897cfe2c6a5f6f7ed345
Sanyam Goel <sanyamgoel10@gmail.com>
2025-03-02 13:06:08 +0530

    config

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 module.exports = {
-    environment: process.env.environment,
+    environment: process.env.environment,   // live for production environment
 
-    port: Number(process.env.port),
+    port: Number(process.env.port),     // Node App Port
     
-    dbUri: process.env.MongoDBUri,
+    dbUri: process.env.MongoDBUri,      // Cloud MongoDB URI (For live environment)
 
-    dbHost: process.env.dbHost,
-    dbPort: Number(process.env.dbPort),
-    dbName: process.env.dbName,
+    dbHost: process.env.dbHost,             // Local MongoDB Host
+    dbPort: Number(process.env.dbPort),     // Local MongoDB Port
+    dbName: process.env.dbName,             // MongoDB Database name
 
-    EncryptSaltRounds: Number(process.env.EncryptSaltRounds),
+    EncryptSaltRounds: Number(process.env.EncryptSaltRounds),   // bcrypt Salt
 
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-    TOKEN_HEADER_KEY: process.env.TOKEN_HEADER_KEY,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,         // JWT Secret
+    TOKEN_HEADER_KEY: process.env.TOKEN_HEADER_KEY,     // JWT Header key
 
-    MaxQuizQuestions: Number(process.env.MaxQuizQuestions),
+    MaxQuizQuestions: Number(process.env.MaxQuizQuestions), // Quiz Questions Configurable
 }
